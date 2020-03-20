e82948b6704c7c55144554f591e28196368d93cc
Luis Marcos Rivera <lmarcos@izertis.com>
2020-03-20 13:32:14 +0100

    SEMANTMURC-42 Paso 1: Creación de importador para todos los xml del primer dataset Añadida importación de los ficheros de grupos de investigación

--- src/main/java/es/um/asio/importer/dataset/config/gruposInvestigacion/ImportGruposInvestigacionDataSetFlowConfiguration.java
+++ src/main/java/es/um/asio/importer/dataset/config/gruposInvestigacion/ImportGruposInvestigacionDataSetFlowConfiguration.java
@@ -5,8 +5,12 @@ import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.job.flow.support.SimpleFlow;
 import org.springframework.context.annotation.Configuration;
 
+import es.um.asio.domain.gruposInvestigacion.ConceptoGrupo;
+import es.um.asio.domain.gruposInvestigacion.ConceptoInvestigador;
 import es.um.asio.domain.gruposInvestigacion.DatosContactoGrupo;
+import es.um.asio.domain.gruposInvestigacion.DatosEquipoInvestigacion;
 import es.um.asio.domain.gruposInvestigacion.GrupoInvestigacion;
+import es.um.asio.domain.gruposInvestigacion.TipoParticipacionGrupo;
 import es.um.asio.importer.dataset.config.ImportDataSetFlowConfigurationBase;
 
 @Configuration
@@ -20,8 +24,12 @@ public class ImportGruposInvestigacionDataSetFlowConfiguration  extends ImportDa
     @Override
     public Flow getFlow() {
         return new FlowBuilder<SimpleFlow>("importGruposInvestigacionFlow")
-                .start(createStep(GrupoInvestigacion.class,"dataset/Grupos de investigación/Grupos de investigacion.xml"))
-                .next(createStep(DatosContactoGrupo.class,"dataset/Grupos de investigación/Datos contacto grupos.xml"))
+                .start(createStep(ConceptoGrupo.class,"dataset/Grupos de investigacion/Conceptos grupos.xml"))
+                .next(createStep(ConceptoInvestigador.class,"dataset/Grupos de investigacion/Conceptos investigadores.xml"))
+                .next(createStep(DatosContactoGrupo.class,"dataset/Grupos de investigacion/Datos contacto grupos.xml"))
+                .next(createStep(DatosEquipoInvestigacion.class,"dataset/Grupos de investigacion/Datos equipo investigacion.xml"))
+                .next(createStep(GrupoInvestigacion.class,"dataset/Grupos de investigacion/Grupos de investigacion.xml"))
+                .next(createStep(TipoParticipacionGrupo.class,"dataset/Grupos de investigacion/Tipo participacion grupo.xml"))                
                 .build();         
     }
 }
