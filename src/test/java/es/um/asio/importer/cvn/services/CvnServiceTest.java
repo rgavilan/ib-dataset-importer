481c2c025f80b1a56d5213fe9eb4e2b5999f0d13
Luis Marcos Rivera <lmarcos@izertis.com>
2020-04-07 09:12:32 +0200

    SEMANTMURC-54 Crear job para llamar a los servicios de cvn de la um Añadidas cabeceras a test

--- src/test/java/es/um/asio/importer/cvn/services/CvnServiceTest.java
+++ src/test/java/es/um/asio/importer/cvn/services/CvnServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.client.ExpectedCount;
@@ -14,10 +13,12 @@ import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.any;
 import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.header;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import java.util.Calendar;
 
@@ -43,19 +44,16 @@ public class CvnServiceTest {
         mockServer = MockRestServiceServer.createServer(restTemplate);
     }
     
-
-    
     @Test
     public void whenfindAllChangesByDateIsCalled_thenReturnsCvnChanges() {
         mockServer.expect(ExpectedCount.once(),
                 requestTo("http://curriculumpruebas.um.es/curriculum/rest/v1/auth/changes?date=2020-03-25"))
                 .andExpect(method(HttpMethod.GET))
-                .andRespond(withStatus(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(givenAJsonWithListOfIds()));              
+                .andExpect(header("application",any(String.class))).andExpect(header("key",any(String.class)))
+                .andRespond(withSuccess(givenAJsonWithListOfIds(),MediaType.APPLICATION_JSON));              
         
         CvnChanges cvnChanges = cvnService.findAllChangesByDate(new Calendar.Builder().setDate(2020, 03 -1 , 25).build().getTime());
-        
+       
         mockServer.verify();
         assertNotNull(cvnChanges);
         assertThat(cvnChanges.getIds()).isNotEmpty();
@@ -66,9 +64,8 @@ public class CvnServiceTest {
         mockServer.expect(ExpectedCount.once(),
                 requestTo("http://curriculumpruebas.um.es/curriculum/rest/v1/auth/changes"))
                 .andExpect(method(HttpMethod.GET))
-                .andRespond(withStatus(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(givenAJsonWithListOfIds()));              
+                .andExpect(header("application",any(String.class))).andExpect(header("key",any(String.class)))
+                .andRespond(withSuccess(givenAJsonWithListOfIds(),MediaType.APPLICATION_JSON));              
         
         CvnChanges cvnChanges = cvnService.findAllChangesByDate(null);
         
@@ -82,9 +79,8 @@ public class CvnServiceTest {
         mockServer.expect(ExpectedCount.once(),
                 requestTo("http://curriculumpruebas.um.es/curriculum/rest/v1/auth/cvn?id=1"))
                 .andExpect(method(HttpMethod.GET))
-                .andRespond(withStatus(HttpStatus.OK)
-                .contentType(MediaType.APPLICATION_XML)
-                .body(givenACvnXmlWithOneCvnItemBean())); 
+                .andExpect(header("application",any(String.class))).andExpect(header("key",any(String.class)))
+                .andRespond(withSuccess(givenACvnXmlWithOneCvnItemBean(),MediaType.APPLICATION_XML));  
         
         Cvn cvn = cvnService.findById(1L);
         
